a73bcde4682711a8ad67f6413c6c01b002f00e5a
Megan Christine McClure <mcclme02@luther.edu>
2020-03-31 00:11:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,21 @@
-# Your code goes here!
-class Dog 
+# # Your code goes here!
+# class Dog 
   
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end 
+#   def name=(dogs_name)
+#     @this_dogs_name = dogs_name
+#   end 
   
-  def name 
-    @this_dogs_name
-  end 
+#   def name 
+#     @this_dogs_name
+#   end 
   
-  def bark
-    puts "woof!"
-  end 
+#   def bark
+#     puts "woof!"
+#   end 
   
-end 
+# end 
 
-# fido = Dog.new 
-# fido.name = "Fido"
+# # fido = Dog.new 
+# # fido.name = "Fido"
 
-# puts fido.bark 
+# # puts fido.bark 
